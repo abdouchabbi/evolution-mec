165dc94bdadfa9b94e634840eff1083fde44af0d
abdou <abdoup4@gmail.com>
2025-09-03 20:15:54 +0200

    Add auth middleware file

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -2,11 +2,9 @@ const asyncHandler = require('express-async-handler');
 const User = require('../models/user.model.js');
 const generateToken = require('../utils/generateToken.js');
 
-/**
- * @desc    تسجيل حساب مستخدم جديد (مدير)
- * @route   POST /api/users/register
- * @access  Public
- */
+// @desc    Register a new user (admin)
+// @route   POST /api/users/register
+// @access  Public
 const registerUser = asyncHandler(async (req, res) => {
     const { name, email, password } = req.body;
 
@@ -36,14 +34,11 @@ const registerUser = asyncHandler(async (req, res) => {
     }
 });
 
-/**
- * @desc    تسجيل دخول المستخدم والحصول على مفتاح وصول
- * @route   POST /api/users/login
- * @access  Public
- */
+// @desc    Auth user & get token
+// @route   POST /api/users/login
+// @access  Public
 const loginUser = asyncHandler(async (req, res) => {
     const { email, password } = req.body;
-
     const user = await User.findOne({ email });
 
     if (user && (await user.matchPassword(password))) {
@@ -59,5 +54,88 @@ const loginUser = asyncHandler(async (req, res) => {
     }
 });
 
-module.exports = { registerUser, loginUser };
+// @desc    Get user profile
+// @route   GET /api/users/profile
+// @access  Private
+const getUserProfile = asyncHandler(async (req, res) => {
+    const user = await User.findById(req.user._id);
+
+    if (user) {
+        res.json({
+            _id: user._id,
+            name: user.name,
+            email: user.email,
+        });
+    } else {
+        res.status(404);
+        throw new Error('المستخدم غير موجود');
+    }
+});
+
+// Other functions...
+const updateUserProfile = asyncHandler(async (req, res) => {
+    const user = await User.findById(req.user._id);
+    if (user) {
+        user.name = req.body.name || user.name;
+        if (req.body.password) {
+            user.password = req.body.password;
+        }
+        const updatedUser = await user.save();
+        res.json({
+            _id: updatedUser._id,
+            name: updatedUser.name,
+            email: updatedUser.email,
+            token: generateToken(updatedUser._id),
+        });
+    } else {
+        res.status(404);
+        throw new Error('المستخدم غير موجود');
+    }
+});
+
+const getUsers = asyncHandler(async (req, res) => {
+    const users = await User.find({}).select('-password');
+    res.json(users);
+});
+
+const deleteUser = asyncHandler(async (req, res) => {
+    const user = await User.findById(req.params.id);
+    if (user) {
+        await user.deleteOne();
+        res.json({ message: 'تم حذف المستخدم بنجاح' });
+    } else {
+        res.status(404);
+        throw new Error('المستخدم غير موجود');
+    }
+});
+
+const updateUser = asyncHandler(async (req, res) => {
+    const user = await User.findById(req.params.id);
+    if (user) {
+        user.name = req.body.name || user.name;
+        if (req.body.password) {
+            user.password = req.body.password;
+        }
+        const updatedUser = await user.save();
+        res.json({
+            _id: updatedUser._id,
+            name: updatedUser.name,
+            email: updatedUser.email,
+        });
+    } else {
+        res.status(404);
+        throw new Error('المستخدم غير موجود');
+    }
+});
+
+
+module.exports = {
+    registerUser,
+    loginUser,
+    getUserProfile,
+    updateUserProfile,
+    getUsers,
+    deleteUser,
+    updateUser,
+};
 
