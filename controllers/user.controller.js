7bdcad8be6021c44d3fcc92661cf6f4ea4e877d0
abdou <abdoup4@gmail.com>
2025-09-05 18:53:12 +0200

    user cont fix

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -6,7 +6,7 @@ const generateToken = require('../utils/generateToken.js');
 // @route   POST /api/users/register
 // @access  Public (should be protected in a real-world scenario after the first admin is created)
 const registerUser = asyncHandler(async (req, res) => {
-    const { name, email, password } = req.body;
+    const { name, email, password, username } = req.body;
 
     if (!name || !email || !password) {
         res.status(400);
@@ -19,7 +19,8 @@ const registerUser = asyncHandler(async (req, res) => {
         throw new Error('المستخدم مسجل بالفعل');
     }
 
-    const user = await User.create({ name, email, password });
+    // Use email as the username to ensure uniqueness
+    const user = await User.create({ name, email, password, username: email });
 
     if (user) {
         res.status(201).json({
@@ -143,4 +144,3 @@ module.exports = {
     deleteUser,
     updateUser,
 };
-
